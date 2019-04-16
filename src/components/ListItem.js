44e0ad669d0ebd4abad2aa7d844199e5820e1a7a
schktjm <si2.o.mjt@gmail.com>
2019-04-16 22:00:37 +0900

    :sparkles: add items

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,12 +1,11 @@
 import React, {useState} from 'react';
 
-const ListItem = () => {
-
-    const [checked, setChecked] = useState(false);
-
+const ListItem = ({text, isFinish}) => {
+    const [checked, setChecked] = useState(isFinish);
     return (
         <div>
-            <input type="checkbox" onClick={() => setChecked(!checked)} defaultChecked={checked}/>list item
+            <input type="checkbox" onClick={() => setChecked(!checked)} defaultChecked={checked}/>
+            {text}
         </div>
     );
 };
