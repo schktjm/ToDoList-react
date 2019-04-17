26cd57f90c1453e1d84a2cb11923b8a8ff9a0223
schktjm <si2.o.mjt@gmail.com>
2019-04-17 17:05:56 +0900

    :+1: del input form and push enter

--- src/components/ListItemForm.js
+++ src/components/ListItemForm.js
@@ -1,11 +1,19 @@
 import React, {useState} from 'react';
 
+
 const ListItemForm = (props) => {
     const [text, setText] = useState('');
+    const sendForm = () => {
+        props.handler(text);
+        setText('');
+    };
     return (
         <div>
-            <input type="text" onChange={(e) => setText(e.target.value)} value={text}/>
-            <button onClick={() => props.handler(text)}>Enter</button>
+            <input type="text" onChange={(e) => setText(e.target.value)} value={text} onKeyDown={(e) => {
+                if (e.keyCode === 13) sendForm();
+            }}/>
+            <button onClick={() => sendForm}>Enter
+            </button>
         </div>
     );
 };
